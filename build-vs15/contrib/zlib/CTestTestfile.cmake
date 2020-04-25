e9bebaaaae4635274ba44739f0ef396f8a67c338
Evan Mortimore <evan-github@madstyle.net>
2020-04-25 10:49:16 -0700

    add build-vs15, even though it's ignored.

---
+++ build-vs15/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/evan/src/madstyle/xclass/3rdParty/assimp/contrib/zlib
+# Build directory: C:/Users/evan/src/madstyle/xclass/3rdParty/assimp/build-vs15/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
